de312b64b8442344dc722d118017c4d380b35c59
Sashakrm <sasakramch@gmail.com>
2025-03-05 09:51:14 +0200

    Add files via upload

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte i = 0, x = 0, y = 0;
+            int[] mas = new int[14];
+            for (i = 0; i < 14; i++)
+                mas[i]= Input(mas,x,i);
+            x = Mas(mas, x, i);
+            Print(x);
+            Console.Read();
+        }
+        static int Input(int[] mas, byte x, byte i)
+        {
+                Console.Write($"mas[{i}]= \t");
+                mas[i] = int.Parse(Console.ReadLine());
+            return mas[i];
+        }
+        static byte Mas(int [] mas, byte x, byte i)
+        {
+            for(i=0;i<14;i++)
+            {
+                if((mas[i]%2)==0)
+                {
+                    x++;
+                }
+            }
+            return x;
+        }
+        static void Print(byte x)
+        {
+            Console.WriteLine($"Всего четных: {x}");
+        }
+    }
+}
